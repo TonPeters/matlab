d1775322edefeb45995c9265c31da31ee05dbfcb
amigo <amigo@cst73>
2015-03-20 11:36:59 +0100

    base testing

---
+++ Base/FRF_base_air/show_single_frf_data.m
@@ -0,0 +1,152 @@
+clear all; 
+close all; 
+clc;
+
+%% on sergio
+filedir = '/home/amigo/ros/data/private/Ton_data/base/frf_air_pos/';
+%% on my pc
+% filedir = '/home/ton/ros/data/private/Ton_data/torso_identification/frf_measurements/';
+
+filename = ['19-03-15_FRF_pos_wheel1_noControl'];
+joint = 2;
+ff = [0.0,0.0];
+
+naverage = 30;
+fs = 1000;
+EQ_LEG = [0.2261, 0.26,   0.26;   0.1761, 0.1905, 0.2109; 0.1206, 0.1352, 0.1667];
+EQ_TRU = [0.0874, 0.0529, 0.0170; 0.0997, 0.0723, 0.0417; 0.1085, 0.0889, 0.0683];
+
+%% import data
+data = importdata([filedir,filename,'.dat']);
+vectorsizes = [4,4,4];
+
+GEARRATIO 			= 9.0/169.0;		
+BITS2CURRENT 		= 25.0/2046.0; 		
+CURRENT2TORQUE 		= 29.2e-3; 			
+BITS2WHEELTORQUE 	= BITS2CURRENT * CURRENT2TORQUE * 1.0/GEARRATIO;
+ENC2RAD = 2.0*3.141592*9.0/169.0/(500.0*4.0);
+
+sample_i = data.data(:,1);
+trace_count = 1;
+signal_count = 1;
+for i=2:1:sum(vectorsizes)+1
+    if signal_count > vectorsizes(trace_count)
+        trace_count = trace_count+1;
+        signal_count = 1;
+    end
+    trace{trace_count}.signal{signal_count} = data.data(:,i);
+    
+    indices = find(trace{trace_count}.signal{signal_count}==-1);
+    if ~isempty(indices), 
+        disp('error, -1 found, tracing not reliable?'); 
+        disp(['number of -1 is ',num2str(length(indices))]);
+        for m=indices
+%             trace{trace_count}.signal{signal_count}(m) = (trace{trace_count}.signal{signal_count}(m-1)+trace{trace_count}.signal{signal_count}(m+1))/2;
+        end
+    end
+    
+    
+    signal_count = signal_count+1;
+end
+time = sample_i;
+
+u1 = trace{1}.signal{1};
+% u2 = trace{1}.signal{2}-ff(2);
+c1 = trace{2}.signal{1};
+% c2 = trace{2}.signal{2};
+err1 = trace{3}.signal{1};
+% err2 = trace{3}.signal{2};
+d1 = u1-c1;
+
+%% create SISO frf
+ei = err1;
+di = d1;
+ui = u1;
+
+shift = 1;
+cut_start = 2;
+cut_end = 2;
+e = ei(cut_start+shift:end-cut_end);
+u = ui(cut_start:end-cut_end-shift);
+d = di(cut_start:end-cut_end-shift);
+%     lp = lpi(1+cut_start:end-cut_end-shift);
+
+% Average
+nfft=round(length(e)/naverage);
+window=hann(nfft);
+noverlap=round(nfft*0.5);
+
+[TrS,hz]=tfestimate(d,u,window,noverlap,nfft,fs);
+[Coh_du,hz]=mscohere(d,u,window,noverlap,nfft,fs);
+[TrPS,hz]=tfestimate(d,e,window,noverlap,nfft,fs);
+[Coh_de,hz]=mscohere(d,e,window,noverlap,nfft,fs);
+
+
+H = -TrPS./(TrS);
+Hfrd = frd(H,hz,'frequencyunit','Hz');
+% 
+% %% Controller
+% Cll = C_leadlag(3.33, 30);
+% Ci = C_integrator(2.0, 1);
+% C = 529.5*Cll*Ci;
+% 
+% CHfrd = C*Hfrd;
+% CH = squeeze(CHfrd.resp);
+
+% figure;
+% nyquist(CHfrd)
+% 0.0, 529.5); # position to volt
+% # var array   GAINS                       = array (0.03, 0.025); # encoder to bits
+% var array   INTEGRATOR_ZERO_FREQUENCY   = array (2.0, 2.0)
+% var array   LEAD_FREQUENCY              = array (3.33, 3.33)
+% var array   LAG_FREQUENCY               = array (30.0, 30.0)
+
+%% plot results
+% create figures
+% h3 = figure; %scr lt;
+% h1 = figure; %scr rt;
+% h2 = figure; %scr rb;
+
+% plot data
+% figure
+figure;
+subplot(3,1,1);
+semilogx(hz,db(H)); hold all;
+subplot(3,1,2);
+semilogx(hz,angle(H).*360/2/pi); hold all;
+subplot(3,1,3)
+semilogx(hz,Coh_de,hz,Coh_du);
+
+% figure(h1)
+% semilogx(hz,abs(Coh_du));
+% hold all;
+% figure(h2)
+% semilogx(hz,abs(Coh_de));
+% hold all;
+
+% figure settings
+% figure(gcf); grid on; title('Coherence d to u'); xlabel('frequency [hz]');
+% ylabel('magnitude abs'); 
+% figure(h2); grid on; title('Coherence d to e'); xlabel('frequency [hz]');
+% ylabel('magnitude abs');      
+% figure(gcf);
+% subplot(2,1,1); grid on; ylabel('Magnitude db');
+% subplot(2,1,2); grid on; ylabel('angle degrees'); xlabel('frequency Hz');
+
+%% load frf
+% load frf/06-02-15_FRF_tr_ce_ce.mat
+% 
+% figure(gcf);
+% subplot(2,1,1);
+% semilogx(hz,db(H)); hold all;
+% subplot(2,1,2);
+% semilogx(hz,angle(H).*360/2/pi); hold all;
+
+%% save frf
+% err = [err1,err2];
+% dd = [d1,d2];
+% cc = [c1,c2];
+% uu = [u1,u2];
+% save(['frf/',filename],'H','hz','err','dd','cc','uu','Coh_du','Coh_de');
+
+all_grids_on();
--- Base/FRF_base_air/single_SISO_frf_air.m
+++ Base/FRF_base_air/single_SISO_frf_air.m
@@ -1,12 +1,27 @@
 % create single frf
 clear all; close all; clc;
 
-filename = '31-10-14_FRF_wheel_air';
-data = importdata([filename,'.dat']);
-vectorsizes = [4, 4, 4];
+%% on sergio
+filedir = '/home/amigo/ros/data/private/Ton_data/base/frf_air_pos/';
+%% on my pc
+% filedir = '/home/ton/ros/data/private/Ton_data/torso_identification/frf_measurements/';
+
+filename = ['19-03-15_FRF_pos_wheel1'];
 wheel = 1;
 
+
+data = importdata([filedir,filename,'.dat']);
+vectorsizes = [4, 4, 4];
+
+
+WHEELRAD 			= 0.075*1.0147; %	# Corrected for effective wheel radius
+HALFSQRT2 			= 0.7071;%
+GEARRATIO 			= 9.0/169.0;	%	# Gearratio Maxxon GP42C
+BITS2CURRENT 		= 50.0/2046.0; 	%	# Bit value to current value (+-2046 bit FPGA-> +-5V ELMO-> +-50A)
+CURRENT2TORQUE 		= 29.2e-3; 		%	# Torque constant of the motor Maxxon RE35
+BITS2WHEELTORQUE 	= BITS2CURRENT * CURRENT2TORQUE * 1.0/GEARRATIO;
 ENC2RAD = 2.0*3.141592*9.0/169.0/(500.0*4.0);
+
 sample_i = data.data(:,1);
 trace_count = 1;
 signal_count = 1;
@@ -40,15 +55,15 @@ figure; scr r;
 subplot(3,1,1)
 plot(time,e); grid on; ylabel('e');
 subplot(3,1,2)
-plot(time,d); grid on;ylabel('d');
+plot(time,d./BITS2WHEELTORQUE); grid on;ylabel('d');
 subplot(3,1,3)
-plot(time,u); grid on;ylabel('u');
+plot(time,u./BITS2WHEELTORQUE); grid on;ylabel('u');
 linkaxes(get(gcf,'children'),'x')
 
 % create frf
 % frf settings
 fs=1000;
-naverage=5;
+naverage=50;
 nfft=round(length(e)/naverage);
 % nfft=1024;
 window=hann(nfft);
@@ -62,16 +77,23 @@ noverlap=round(nfft*0.5);
 H=-TrPS./TrS;
 
 figure; scr r;
-subplot(3,1,1); 
+semilogx(hz,db(TrPS)); grid on;
+ylabel('Magnitude dB'); hold all;
+
+
+figure; scr r;
+subplot(4,1,1); 
 semilogx(hz,db(H)); grid on;
 ylabel('Magnitude dB'); hold all;
-subplot(3,1,2); 
+subplot(4,1,2); 
 semilogx(hz,angle(H).*(360/2/pi));grid on;
 ylabel('Phase degrees'); xlabel('Frequency [Hz]');hold all;
-subplot(3,1,3); 
+subplot(4,1,3); 
 semilogx(hz,(Trdu),hz,(Trde)); grid on;
 ylabel('Magnitude'); title('coherence'); legend('d-u','d-e');
-
+subplot(4,1,4); 
+semilogx(hz,db(TrPS),hz,db(TrS)); grid on;
+ylabel('Magnitude dB'); hold all;
 linkaxes(get(gcf,'children'),'x')
 
 % save(['frf_wheel',num2str(wheel),'_air.mat'],'H','hz');
